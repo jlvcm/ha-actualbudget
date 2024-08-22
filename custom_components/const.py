aa0d0d1a06d0e7bcc8946bb9b1aa73b2ded5885b
jlvcm <joaolvcm@gmail.com>
2024-08-22 20:15:19 +0100

    initial commit

---
+++ custom_components/const.py
@@ -0,0 +1,10 @@
+DOMAIN = "actualbudget"
+PLATFORM = "sensor"
+DOMAIN_DATA = f"{DOMAIN}_data"
+
+DEFAULT_ICON = "mdi:bank"
+UNIT_OF_MEASUREMENT = "€"
+
+CONFIG_ENDPOINT = 'endpoint'
+CONFIG_PASSWORD = 'password'
+
