a459147ce23c7a53da5384781db7f69b35ea7475
Morten <32544273+Fywqdk@users.noreply.github.com>
2025-01-14 13:56:23 +0100

    Update config_flow.py
    Reverting changes for now

--- custom_components/actualbudget/config_flow.py
+++ custom_components/actualbudget/config_flow.py
@@ -3,11 +3,10 @@
 from __future__ import annotations
 
 import logging
-
 import voluptuous as vol
 from urllib.parse import urlparse
 
-from homeassistant import config_entries, core, exceptions
+from homeassistant import config_entries
 from homeassistant.helpers.selector import selector
 
 from .actualbudget import ActualBudget
@@ -48,54 +47,39 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle a flow initialized by the user interface."""
         _LOGGER.debug("Starting async_step_user...")
 
-        errors = {}
-        if user_input is not None:
-            try:
-                unique_id = (
-                    user_input[CONFIG_ENDPOINT].lower() + "_" + user_input[CONFIG_FILE].lower()
-                )
-                endpoint = user_input[CONFIG_ENDPOINT]
-                domain = urlparse(endpoint).hostname
-                port = urlparse(endpoint).port
-                password = user_input[CONFIG_PASSWORD]
-                file = user_input[CONFIG_FILE]
-                cert = user_input.get(CONFIG_CERT)
-                encrypt_password = user_input.get(CONFIG_ENCRYPT_PASSWORD)
-                if cert == "SKIP":
-                    cert = False
-        
-                await self.async_set_unique_id(unique_id)
-                self._abort_if_unique_id_configured()
-        
-                await self._test_connection(
-                    endpoint, password, file, cert, encrypt_password
-                )
-                
-                return self.async_create_entry(
-                    title=f"{domain}:{port} {file}",
-                    data=user_input,
-                )
-            
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except InvalidAuth:
-                errors["base"] = "invalid_auth"
-            except Exception:
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
+        if user_input is None:
+            return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
 
-            
+        unique_id = (
+            user_input[CONFIG_ENDPOINT].lower() + "_" + user_input[CONFIG_FILE].lower()
+        )
+        endpoint = user_input[CONFIG_ENDPOINT]
+        domain = urlparse(endpoint).hostname
+        port = urlparse(endpoint).port
+        password = user_input[CONFIG_PASSWORD]
+        file = user_input[CONFIG_FILE]
+        cert = user_input.get(CONFIG_CERT)
+        encrypt_password = user_input.get(CONFIG_ENCRYPT_PASSWORD)
+        if cert == "SKIP":
+            cert = False
+
+        await self.async_set_unique_id(unique_id)
+        self._abort_if_unique_id_configured()
+
+        error = await self._test_connection(
+            endpoint, password, file, cert, encrypt_password
+        )
+        if error:
             return self.async_show_form(
-                step_id="user", data_schema=DATA_SCHEMA, errors=errors
+                step_id="user", data_schema=DATA_SCHEMA, errors={"base": error}
+            )
+        else:
+            return self.async_create_entry(
+                title=f"{domain}:{port} {file}",
+                data=user_input,
             )
 
     async def _test_connection(self, endpoint, password, file, cert, encrypt_password):
         """Return true if gas station exists."""
         api = ActualBudget(self.hass, endpoint, password, file, cert, encrypt_password)
         return await api.test_connection()
-
-class CannotConnect(exceptions.HomeAssistantError):
-    """Error to indicate we cannot connect."""
-
-class InvalidAuth(exceptions.HomeAssistantError):
-    """Error to indicate there is invalid auth."""
