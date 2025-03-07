e58cef5b92134ac22bf22a5df721c780c23157bb
Joep Driesen <joep.driesen@nccn.fgov.be>
2025-03-07 13:10:48 +0100

    Move thread locking to _sync methods

--- custom_components/actualbudget/actualbudget.py
+++ custom_components/actualbudget/actualbudget.py
@@ -60,32 +60,31 @@ class ActualBudget:
 
     def get_session(self):
         """Get Actual session if it exists, or create a new one safely."""
-        with self._lock:  # Ensure only one thread enters at a time
-            # Invalidate session if it is too old
-            if (
-                self.actual
-                and self.sessionStartedAt + SESSION_TIMEOUT < datetime.datetime.now()
-            ):
-                try:
-                    self.actual.__exit__(None, None, None)
-                except Exception as e:
-                    _LOGGER.error("Error closing session: %s", e)
+        # Invalidate session if it is too old
+        if (
+            self.actual
+            and self.sessionStartedAt + SESSION_TIMEOUT < datetime.datetime.now()
+        ):
+            try:
+                self.actual.__exit__(None, None, None)
+            except Exception as e:
+                _LOGGER.error("Error closing session: %s", e)
+            self.actual = None
+
+        # Validate existing session
+        if self.actual:
+            try:
+                result = self.actual.validate()
+                if not result.data.validated:
+                    raise Exception("Session not validated")
+            except Exception as e:
+                _LOGGER.error("Error validating session: %s", e)
                 self.actual = None
 
-            # Validate existing session
-            if self.actual:
-                try:
-                    result = self.actual.validate()
-                    if not result.data.validated:
-                        raise Exception("Session not validated")
-                except Exception as e:
-                    _LOGGER.error("Error validating session: %s", e)
-                    self.actual = None
-
-            # Create a new session if needed
-            if not self.actual:
-                self.actual = self.create_session()
-                self.sessionStartedAt = datetime.datetime.now()
+        # Create a new session if needed
+        if not self.actual:
+            self.actual = self.create_session()
+            self.sessionStartedAt = datetime.datetime.now()
 
         return self.actual.session  # Return session after lock is released
 
@@ -109,9 +108,10 @@ class ActualBudget:
         return await self.hass.async_add_executor_job(self.get_accounts_sync)
 
     def get_accounts_sync(self) -> List[Account]:
-        session = self.get_session()
-        accounts = get_accounts(session)
-        return [Account(name=a.name, balance=a.balance) for a in accounts]
+        with self._lock:  # Ensure only one thread enters at a time
+            session = self.get_session()
+            accounts = get_accounts(session)
+            return [Account(name=a.name, balance=a.balance) for a in accounts]
 
     async def get_account(self, account_name) -> Account:
         return await self.hass.async_add_executor_job(
@@ -123,40 +123,42 @@ class ActualBudget:
         self,
         account_name,
     ) -> Account:
-        session = self.get_session()
-        account = get_account(session, account_name)
-        if not account:
-            raise Exception(f"Account {account_name} not found")
-        return Account(name=account.name, balance=account.balance)
+        with self._lock:  # Ensure only one thread enters at a time
+            session = self.get_session()
+            account = get_account(session, account_name)
+            if not account:
+                raise Exception(f"Account {account_name} not found")
+            return Account(name=account.name, balance=account.balance)
 
     async def get_budgets(self) -> List[Budget]:
         """Get budgets."""
         return await self.hass.async_add_executor_job(self.get_budgets_sync)
 
     def get_budgets_sync(self) -> List[Budget]:
-        session = self.get_session()
-        budgets_raw = get_budgets(session)
-        budgets: Dict[str, Budget] = {}
-        for budget_raw in budgets_raw:
-            if not budget_raw.category:
-                continue
-            category = str(budget_raw.category.name)
-            amount = None if not budget_raw.amount else (float(budget_raw.amount) / 100)
-            month = str(budget_raw.month)
-            if category not in budgets:
-                budgets[category] = Budget(
-                    name=category, amounts=[], balance=Decimal(0)
+        with self._lock:  # Ensure only one thread enters at a time
+            session = self.get_session()
+            budgets_raw = get_budgets(session)
+            budgets: Dict[str, Budget] = {}
+            for budget_raw in budgets_raw:
+                if not budget_raw.category:
+                    continue
+                category = str(budget_raw.category.name)
+                amount = None if not budget_raw.amount else (float(budget_raw.amount) / 100)
+                month = str(budget_raw.month)
+                if category not in budgets:
+                    budgets[category] = Budget(
+                        name=category, amounts=[], balance=Decimal(0)
+                    )
+                budgets[category].amounts.append(BudgetAmount(month=month, amount=amount))
+            for category in budgets:
+                budgets[category].amounts = sorted(
+                    budgets[category].amounts, key=lambda x: x.month
                 )
-            budgets[category].amounts.append(BudgetAmount(month=month, amount=amount))
-        for category in budgets:
-            budgets[category].amounts = sorted(
-                budgets[category].amounts, key=lambda x: x.month
-            )
-            category_data = get_category(session, category)
-            budgets[category].balance = (
-                category_data.balance if category_data else Decimal(0)
-            )
-        return list(budgets.values())
+                category_data = get_category(session, category)
+                budgets[category].balance = (
+                    category_data.balance if category_data else Decimal(0)
+                )
+            return list(budgets.values())
 
     async def get_budget(self, budget_name) -> Budget:
         return await self.hass.async_add_executor_job(
@@ -168,19 +170,20 @@ class ActualBudget:
         self,
         budget_name,
     ) -> Budget:
-        session = self.get_session()
-        budgets_raw = get_budgets(session, None, budget_name)
-        if not budgets_raw or not budgets_raw[0]:
-            raise Exception(f"budget {budget_name} not found")
-        budget: Budget = Budget(name=budget_name, amounts=[], balance=Decimal(0))
-        for budget_raw in budgets_raw:
-            amount = None if not budget_raw.amount else (float(budget_raw.amount) / 100)
-            month = str(budget_raw.month)
-            budget.amounts.append(BudgetAmount(month=month, amount=amount))
-        budget.amounts = sorted(budget.amounts, key=lambda x: x.month)
-        category_data = get_category(session, budget_name)
-        budget.balance = category_data.balance if category_data else Decimal(0)
-        return budget
+        with self._lock:  # Ensure only one thread enters at a time
+            session = self.get_session()
+            budgets_raw = get_budgets(session, None, budget_name)
+            if not budgets_raw or not budgets_raw[0]:
+                raise Exception(f"budget {budget_name} not found")
+            budget: Budget = Budget(name=budget_name, amounts=[], balance=Decimal(0))
+            for budget_raw in budgets_raw:
+                amount = None if not budget_raw.amount else (float(budget_raw.amount) / 100)
+                month = str(budget_raw.month)
+                budget.amounts.append(BudgetAmount(month=month, amount=amount))
+            budget.amounts = sorted(budget.amounts, key=lambda x: x.month)
+            category_data = get_category(session, budget_name)
+            budget.balance = category_data.balance if category_data else Decimal(0)
+            return budget
 
     async def test_connection(self):
         return await self.hass.async_add_executor_job(self.test_connection_sync)
